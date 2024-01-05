84015996344dfc74f8a69054e58c52d1b163c1e3
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-05 10:25:33 -0300

    Transformando com o Map

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { constant } from 'lodash';
-import { Observable, tap } from 'rxjs';
-import { LivrosResultado } from '../models/interfaces';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,12 @@ export class LivroService {
 
   constructor(private http: HttpClient) { }
 
-  buscar (valorDigitado: string): Observable<LivrosResultado> {
-      const params = new HttpParams().append('q', valorDigitado)
-      return this.http.get<LivrosResultado>(this.API, {params}).pipe(
-        tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
+  buscar(valorDigitado: string): Observable<Item[]> {
+    const params = new HttpParams().append('q', valorDigitado )
+    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+      tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
+      map(resultado => resultado.items),
+      tap(resultado => console.log('Fluxo após o map', resultado))
       )
   }
 }

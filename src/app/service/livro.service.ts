4789cb37690a40da2413d2377dc95556d892bd87
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-05 15:32:02 -0300

    Adicionando o SwitchMap

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { constant } from 'lodash';
-import { Observable, map, tap } from 'rxjs';
-import { Item, LivrosResultado } from '../models/interfaces';
+import { Observable } from 'rxjs';
+import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +9,11 @@ import { Item, LivrosResultado } from '../models/interfaces';
 export class LivroService {
 
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
-
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado )
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
-      map(resultado => resultado.items),
-      tap(resultado => console.log('Fluxo após o map', resultado))
-      )
+    return this.http.get<LivrosResultado>(this.API, { params })
   }
 }
+
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,41 +1,66 @@
-import { Subscription } from 'rxjs';
-import { Component, OnDestroy } from '@angular/core';
+import { switchMap, map, tap, filter, debounceTime, throwError, catchError, EMPTY, of } from 'rxjs';
+import { Component } from '@angular/core';
 import { LivroService } from 'src/app/service/livro.service';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent {
 
-  listaLivros: Livro [];
-  campoBusca: string = ''
-  subscription: Subscription
-  livro: Livro
+  campoBusca = new FormControl();
+  mensagemErro = ''
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) { }
 
-  buscarLivros() {
-    this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
-      },
-      error: erro => console.error(erro),
-    }
+  // totalDeLivros$ = this.campoBusca.valueChanges
+  // .pipe(
+  //   debounceTime(PAUSA),
+  //   filter((valorDigitado) => valorDigitado.length >= 3),
+  //   tap(() => console.log('Fluxo inicial')),
+  //   switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+  //   map(resultado => this.livrosResultado = resultado),
+  //   catchError(erro => {
+  //     console.log(erro)
+  //     return of()
+  //   })
+  // )
+
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
+      tap((retornoAPI) => console.log(retornoAPI)),
+      map(resultado => resultado.items ?? []),
+      map((items) => this.livrosResultadoParaLivros(items)),
+      catchError((erro) => {
+        // this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'
+        // return EMPTY
+        console.log(erro)
+        return throwError(() => new Error(this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'))
+      })
     )
-  }
+
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
-    return items.map(item =>{
+    return items.map(item => {
       return new LivroVolumeInfo(item)
     })
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe()
-  }
 }
 
 
+
+function trhowError(arg0: () => Error) {
+  throw new Error('Function not implemented.');
+}

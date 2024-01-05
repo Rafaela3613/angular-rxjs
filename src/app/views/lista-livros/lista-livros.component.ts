d9cb40c0d9fc86d6ca23c8c86b767c8f9ba6f105
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-05 15:41:20 -0300

    adicionando o Pipe async

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -20,21 +20,7 @@ export class ListaLivrosComponent {
 
   constructor(private service: LivroService) { }
 
-  // totalDeLivros$ = this.campoBusca.valueChanges
-  // .pipe(
-  //   debounceTime(PAUSA),
-  //   filter((valorDigitado) => valorDigitado.length >= 3),
-  //   tap(() => console.log('Fluxo inicial')),
-  //   switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-  //   map(resultado => this.livrosResultado = resultado),
-  //   catchError(erro => {
-  //     console.log(erro)
-  //     return of()
-  //   })
-  // )
-
-  livrosEncontrados$ = this.campoBusca.valueChanges
-    .pipe(
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
       debounceTime(PAUSA),
       filter((valorDigitado) => valorDigitado.length >= 3),
       tap(() => console.log('Fluxo inicial')),
@@ -44,8 +30,6 @@ export class ListaLivrosComponent {
       map(resultado => resultado.items ?? []),
       map((items) => this.livrosResultadoParaLivros(items)),
       catchError((erro) => {
-        // this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'
-        // return EMPTY
         console.log(erro)
         return throwError(() => new Error(this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'))
       })
@@ -58,8 +42,6 @@ export class ListaLivrosComponent {
   }
 
 }
-
-
 
 function trhowError(arg0: () => Error) {
   throw new Error('Function not implemented.');
